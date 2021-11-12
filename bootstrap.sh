ae2d58efb221e8d8f34b3839dbec1acb5b36566d
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-11-12 16:31:23 +0000

    add SSM instance profile to prevent premature shutdown of EC2 instances

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,7 @@ set -x
 
 
 aws cloudformation deploy \
+    --capabilities CAPABILITY_NAMED_IAM \
     --template-file cloud-formation.yaml \
     --stack-name temporary-stack \
     --parameter-overrides \
