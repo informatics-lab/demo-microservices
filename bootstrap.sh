d1a8ded9f970ac3dc4ff01b1ad08fa8209ce93c2
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-11-16 16:15:16 +0000

    simplify stack

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ set -x
 aws cloudformation deploy \
     --capabilities CAPABILITY_NAMED_IAM \
     --template-file cloud-formation.yaml \
-    --stack-name temporary-stack \
+    --stack-name practice-stack \
     --parameter-overrides \
-        KeyName=andy
+        KeyName=andy \
+        ServiceName=wildfire
