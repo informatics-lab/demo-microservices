6e19f2ac56c39a35f77077025c3caf644679fd5e
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-11-17 17:04:31 +0000

    export values from network stack

--- launch.sh
+++ launch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
 
+TEMPLATE_FILE=${1}
 
 aws cloudformation deploy \
     --capabilities CAPABILITY_NAMED_IAM \
-    --template-file ec2-instance.yaml \
-    --stack-name practice-ec2-stack \
+    --template-file ${TEMPLATE_FILE} \
+    --stack-name practice-stack \
     --parameter-overrides \
         KeyName=andy
