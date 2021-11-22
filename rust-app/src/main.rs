72b7c79ba016f3980875a23fb712dbb9624be661
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-11-22 11:25:59 +0000

    add geojson endpoint to api service

--- rust-app/src/main.rs
+++ rust-app/src/main.rs
@@ -1,5 +1,8 @@
 #[macro_use] extern crate rocket;
 use rocket::serde::{Serialize, json::Json};
+use geojson::FeatureCollection;
+use geo_types::{GeometryCollection, LineString, Polygon};
+use std::iter::FromIterator;
 
 #[derive(Serialize)]
 struct Task {
@@ -11,8 +14,19 @@ fn api() -> Json<Task> {
     Json(Task { number: 42 })
 }
 
+#[get("/collection")]
+fn collection() -> Json<FeatureCollection> {
+    let polygon = Polygon::new(
+        LineString::from(vec![(0., 0.), (1., 1.), (1., 0.), (0., 0.)]),
+        vec![],
+    );
+    let geometry = GeometryCollection::from_iter(vec![polygon]);
+    let feature = FeatureCollection::from(&geometry);
+    Json(feature)
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![api])
+        .mount("/", routes![api, collection])
 }
